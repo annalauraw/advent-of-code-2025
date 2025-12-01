83858f4fc3dade035a891d4c3c3fc116cf52c106
Annabelle Wiegart <annabelle.wiegart@zb.uzh.ch>
2025-12-01 17:48:17 +0100

    Initial commit

---
+++ day_1.py
@@ -0,0 +1,101 @@
+import re
+
+
+def _get_dial_position(number):
+    """
+    Get a number between 0 and 99.
+    """
+    try:
+        assert 0 <= number <= 99
+        return number
+    except AssertionError:
+        if number > 99:
+            number = number - 100
+        elif number < 0:
+            number = 100 + number
+        return _get_dial_position(number)
+
+
+# def _zero_crossings(number):
+#     """
+#     Get the number of times zero is crossed to get to the.
+#     """
+#     zero_crossings = 0
+#     try:
+#         assert 0 <= number <= 99
+#         return zero_crossings
+#     except AssertionError:
+#         if number > 99:
+#             number = number - 100
+#         elif number < 0:
+#             number = 100 + number
+#         return _get_dial_position(number)
+
+
+def _get_direction_and_amount(operation: str):
+    matches = re.search(r"(R|L)(\d+)", operation)
+    if matches:
+        direction = matches.group(1)
+        amount = int(matches.group(2))
+        return direction, amount
+
+
+def get_next_dial_position(start_position: int, operation: str):
+    direction, amount = _get_direction_and_amount(operation)
+    if direction == "R":
+        new_number = start_position + amount
+    elif direction == "L":
+        new_number = start_position - amount
+
+    new_position = _get_dial_position(new_number)
+    return new_position
+
+
+def get_number_of_zero_crossings(start_position: int, operation: str):
+    zero_crossings = 0
+
+    direction, amount = _get_direction_and_amount(operation)
+    roughly = amount // 100
+    zero_crossings += roughly
+    modulo = amount % 100
+    if direction == "R":
+        modulo_addition = start_position + modulo
+    elif direction == "L":
+        modulo_addition = start_position - modulo
+
+    if not 0 <= modulo_addition <= 99:
+        zero_crossings += 1
+
+    print(f"Start position: {str(start_position)}")
+    print(f"Operation: {operation}")
+    print(f"Zero crossings: {str(zero_crossings)}")
+
+    return zero_crossings
+
+
+if __name__ == "__main__":
+
+    start_position = 50
+    zero_counter = 0
+
+    with open("input/day_1_input.txt", mode="rt", encoding="utf-8") as inputfile:
+        operations = inputfile.readlines()
+
+    print(f"Input file contains {str(len(operations))} operations.")
+
+    for op in operations:
+
+        next_dial_position = get_next_dial_position(start_position, op)
+        if next_dial_position == 0:
+            # print(f"Starting position: {str(start_position)}")
+            # print(f"Operation: {op}")
+            # print(f"New position: {str(next_dial_position)}")
+            zero_counter += 1
+
+        # optionally: get all zero crossings
+        additional_zero_crossings = get_number_of_zero_crossings(start_position, op)
+        zero_counter += additional_zero_crossings
+
+        start_position = next_dial_position
+
+    print(str(zero_counter))
