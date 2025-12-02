94be3ead7c536990fc17f48d53903553d7d472f0
Annabelle Wiegart <annabelle.wiegart@zb.uzh.ch>
2025-12-02 10:31:37 +0100

    Get zero crossings in _get_dial_position()

--- day_1.py
+++ day_1.py
@@ -1,35 +1,20 @@
 import re
 
 
-def _get_dial_position(number):
+def _get_dial_position(number, zero_crossings: int = 0):
     """
     Get a number between 0 and 99.
     """
     try:
         assert 0 <= number <= 99
-        return number
+        return number, zero_crossings
     except AssertionError:
+        zero_crossings += 1
         if number > 99:
             number = number - 100
         elif number < 0:
             number = 100 + number
-        return _get_dial_position(number)
-
-
-# def _zero_crossings(number):
-#     """
-#     Get the number of times zero is crossed to get to the.
-#     """
-#     zero_crossings = 0
-#     try:
-#         assert 0 <= number <= 99
-#         return zero_crossings
-#     except AssertionError:
-#         if number > 99:
-#             number = number - 100
-#         elif number < 0:
-#             number = 100 + number
-#         return _get_dial_position(number)
+        return _get_dial_position(number, zero_crossings)
 
 
 def _get_direction_and_amount(operation: str):
@@ -47,30 +32,30 @@ def get_next_dial_position(start_position: int, operation: str):
     elif direction == "L":
         new_number = start_position - amount
 
-    new_position = _get_dial_position(new_number)
-    return new_position
+    new_position, zero_crossings = _get_dial_position(new_number)
+    return new_position, zero_crossings
 
 
-def get_number_of_zero_crossings(start_position: int, operation: str):
-    zero_crossings = 0
+# def get_number_of_zero_crossings(start_position: int, operation: str):
+#     zero_crossings = 0
 
-    direction, amount = _get_direction_and_amount(operation)
-    roughly = amount // 100
-    zero_crossings += roughly
-    modulo = amount % 100
-    if direction == "R":
-        modulo_addition = start_position + modulo
-    elif direction == "L":
-        modulo_addition = start_position - modulo
+#     direction, amount = _get_direction_and_amount(operation)
+#     roughly = amount // 100
+#     zero_crossings += roughly
+#     modulo = amount % 100
+#     if direction == "R":
+#         modulo_addition = start_position + modulo
+#     elif direction == "L":
+#         modulo_addition = start_position - modulo
 
-    if not 0 <= modulo_addition <= 99:
-        zero_crossings += 1
+#     if not 0 <= modulo_addition <= 99:
+#         zero_crossings += 1
 
-    print(f"Start position: {str(start_position)}")
-    print(f"Operation: {operation}")
-    print(f"Zero crossings: {str(zero_crossings)}")
+#     print(f"Start position: {str(start_position)}")
+#     print(f"Operation: {operation}")
+#     print(f"Zero crossings: {str(zero_crossings)}")
 
-    return zero_crossings
+#     return zero_crossings
 
 
 if __name__ == "__main__":
@@ -85,16 +70,17 @@ if __name__ == "__main__":
 
     for op in operations:
 
-        next_dial_position = get_next_dial_position(start_position, op)
+        next_dial_position, zero_crossings = get_next_dial_position(start_position, op)
+        print(f"Starting position: {str(start_position)}")
+        print(f"Operation: {op}")
+        print(f"New position: {str(next_dial_position)}")
+        print(f"Zero crossings: {str(zero_crossings)}")
         if next_dial_position == 0:
-            # print(f"Starting position: {str(start_position)}")
-            # print(f"Operation: {op}")
-            # print(f"New position: {str(next_dial_position)}")
             zero_counter += 1
 
         # optionally: get all zero crossings
-        additional_zero_crossings = get_number_of_zero_crossings(start_position, op)
-        zero_counter += additional_zero_crossings
+        # zero_crossings = get_number_of_zero_crossings(start_position, op)
+        zero_counter += zero_crossings
 
         start_position = next_dial_position
 
