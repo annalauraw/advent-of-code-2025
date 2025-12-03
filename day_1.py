16f9c9ee3ff50bc08ddeb28c9f72bb471695e3ce
Annabelle Wiegart <annabelle.wiegart@zb.uzh.ch>
2025-12-03 08:17:22 +0100

    Prevent double-counting zeros when turning left

--- day_1.py
+++ day_1.py
@@ -9,7 +9,8 @@ def _get_dial_position(number, zero_crossings: int = 0):
         assert 0 <= number <= 99
         return number, zero_crossings
     except AssertionError:
-        zero_crossings += 1
+        if number != 100:
+            zero_crossings += 1
         if number > 99:
             number = number - 100
         elif number < 0:
@@ -27,35 +28,17 @@ def _get_direction_and_amount(operation: str):
 
 def get_next_dial_position(start_position: int, operation: str):
     direction, amount = _get_direction_and_amount(operation)
-    if direction == "R":
-        new_number = start_position + amount
-    elif direction == "L":
-        new_number = start_position - amount
+    if direction == "L":
+        amount = -amount
+    new_number = start_position + amount
 
     new_position, zero_crossings = _get_dial_position(new_number)
+
+    # prevent double-counting with L-operations
+    if start_position == 0 and direction == "L":
+        zero_crossings -= 1
+
     return new_position, zero_crossings
-
-
-# def get_number_of_zero_crossings(start_position: int, operation: str):
-#     zero_crossings = 0
-
-#     direction, amount = _get_direction_and_amount(operation)
-#     roughly = amount // 100
-#     zero_crossings += roughly
-#     modulo = amount % 100
-#     if direction == "R":
-#         modulo_addition = start_position + modulo
-#     elif direction == "L":
-#         modulo_addition = start_position - modulo
-
-#     if not 0 <= modulo_addition <= 99:
-#         zero_crossings += 1
-
-#     print(f"Start position: {str(start_position)}")
-#     print(f"Operation: {operation}")
-#     print(f"Zero crossings: {str(zero_crossings)}")
-
-#     return zero_crossings
 
 
 if __name__ == "__main__":
@@ -75,11 +58,11 @@ if __name__ == "__main__":
         print(f"Operation: {op}")
         print(f"New position: {str(next_dial_position)}")
         print(f"Zero crossings: {str(zero_crossings)}")
+        # part 1
         if next_dial_position == 0:
             zero_counter += 1
 
-        # optionally: get all zero crossings
-        # zero_crossings = get_number_of_zero_crossings(start_position, op)
+        # part 2
         zero_counter += zero_crossings
 
         start_position = next_dial_position
